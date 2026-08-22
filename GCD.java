2e8b1bb1e227be902dbdc907d91820983fe558b5
gamerpixel387-crypto <gamerpixel387@gmail.com>
2026-08-22 09:56:10 +0530

    more answe

---
+++ GCD.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int gcd = findGCD(num1, num2);
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+    }
+
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    
+}
