9effce96c0119e0c6a644cabc00717f1773aec74
gamerpixel387-crypto <gamerpixel387@gmail.com>
2026-08-22 09:58:31 +0530

    more answe

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Armstrong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+}
