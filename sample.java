19e8cc84c4609739d12c073e7938891c1f9052cf
gamerpixel387-crypto <gamerpixel387@gmail.com>
2026-08-22 08:53:34 +0530

    this is day3 program

---
+++ sample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Sample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println("You entered: " + number);
+    }
+}
